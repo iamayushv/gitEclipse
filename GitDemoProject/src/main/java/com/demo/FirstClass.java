e202fa6958a1cbbc285eeae8c477110d45af9ba7
ADMIN <av@ayushv.com>
2019-01-17 10:55:10 +0530

    First commit

--- GitDemoProject/src/main/java/com/demo/FirstClass.java
+++ GitDemoProject/src/main/java/com/demo/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Maven Project on git");
+		System.out.println("Changes made in first commit");
 	}
 
 }
