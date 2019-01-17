3f6b8dad359ab3ecd774028e639a6475a66e32fa
ayushof <ayush@off.com>
2019-01-17 19:32:18 +0530

    Adding New Class to develop

--- GitDemoProject/src/main/java/com/demo/FirstClass.java
+++ GitDemoProject/src/main/java/com/demo/FirstClass.java
@@ -6,6 +6,8 @@ public class FirstClass {
 		// TODO Auto-generated method stub
 		System.out.println("First Maven Project on git");
 		System.out.println("Changes made in first commit");
+		System.out.println("Added another print line");
+		SecondClass newClass=new SecondClass();
 	}
 
 }
