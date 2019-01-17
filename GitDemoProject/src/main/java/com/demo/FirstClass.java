1183374c7cbbdba47309354b322b7739dec7fcc5
Ayush Varshney <ayushvcool@gmail.com>
2019-01-17 10:47:52 +0530

    Add files via upload
    Master pushed

---
+++ GitDemoProject/src/main/java/com/demo/FirstClass.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Maven Project on git");
+	}
+
+}
